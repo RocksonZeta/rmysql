aff175a94b083696b343891363a0a6ae840cde74
zhangdaqian <zhangdaqian@iqidao.com>
2019-04-05 22:40:30 +0800

    first commit

--- MysqlService_test.go
+++ MysqlService_test.go
@@ -30,7 +30,7 @@ func init() {
 // var conStr = "admin:sZlryBOgLxAuAtx9@tcp(test.iqidao.com:43120)/iqidao2?charset=utf8mb4"
 
 func TestSelect(t *testing.T) {
-	my := rmysql.NewMysqlService(conf.Mysql, "iqidao", true)
+	my := rmysql.NewMysqlService(conf.Mysql, "test", true)
 	defer my.Close()
 	r := my.SelectInt("select count(*) from User where id>?", 100)
 	fmt.Println(r)
